5d35edd07e8ee0f3e5dc7b596611889dabe79e93
Santiago Sica <santisica29@gmail.com>
2024-12-29 22:35:28 -0300

    made function cleaner

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
-    
-    let choice = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
 
-    if (choice == 0) return "rock";
-    if (choice == 1) return "paper";
-    if (choice == 2) return "scissors";
+  if (choice == 0) return "rock";
+  if (choice == 1) return "paper";
+  if (choice == 2) return "scissors";
 }
 
-function getHumanChoice(){
-    let flag = false;
-    let userInput;
+function getHumanChoice() {
+  let flag = false;
+  let userInput;
 
-    while (!flag){
-        userInput = prompt("Choose rock, paper or scissors").toLowerCase();
+  while (!flag) {
+    userInput = prompt("Choose rock, paper or scissors").toLowerCase();
 
-        if (userInput === "rock" || userInput === "scissors" || userInput === "paper"){
-            flag = true;
-        } 
+    if (
+      userInput === "rock" ||
+      userInput === "scissors" ||
+      userInput === "paper"
+    ) {
+      flag = true;
     }
+  }
 
-    return userInput;
+  return userInput;
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice) {
+  let won =
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper");
 
-    if (humanChoice === "rock" && computerChoice === "scissors"){
-        humanScore++;
-        return "You win, rock beats paper" 
-    } 
-    
-    return "You lose " + computerChoice;
+  let tie = humanChoice === computerChoice;
+  if (won) {
+    humanScore++;
+    return "You win, rock beats paper";
+  }
+
+  return "You lose " + computerChoice;
 }
 
 console.log(playRound(getHumanChoice(), getComputerChoice()));
